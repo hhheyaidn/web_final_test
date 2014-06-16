98b99f015e394cf5c190fa9732fbd03c4820f7fb
hhheyaidn <hhheyaidn26@gmail.com>
2014-06-16 18:34:08 +0800

    test

--- js/farmer_list.js
+++ js/farmer_list.js
@@ -30,7 +30,27 @@ $(function(){
 });
 
 function getData(page,category){
+	
+	$('ul.tabs li').click(function() {
+		// 找出 li 中的超連結 href(#id)
+	
+		var $this = $(this),
+			_clickTab = $this.find('a').attr('href');
+			
+			console.log($this.li);
+		// 把目前點擊到的 li 頁籤加上 .active
+		// 並把兄弟元素中有 .active 的都移除 class
+		$this.addClass('active').siblings('.active').removeClass('active');
+		// 淡入相對應的內容並隱藏兄弟元素
+		$(_clickTab).stop(false, true).fadeIn().siblings().hide();
+ 
+		return false;
+	}).find('a').focus(function(){
+		this.blur();
+	});
+	
 
+	
   var limit = 15;
   var skip = (page-1) * limit;
   var Farmer = Parse.Object.extend("Farmer");
