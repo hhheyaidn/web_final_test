ec1c8168787b2b225ce4ed286ab1bfc1a1060c72
hhheyaidn <hhheyaidn26@gmail.com>
2014-06-17 00:17:58 +0800

    test

--- js/farmer_list.js
+++ js/farmer_list.js
@@ -65,7 +65,7 @@ function getData(page,category){
         //var photo = e.Farmer_Pic;
         //$("profileImg")[0].src = profilePhoto.url();
         //var html = '<a href="farmer.html"><img src="img/about_2.png"></img></a><a href="farmer.html" id="name">'+e.Name+'</a><br><a href="farmer.html" id="product">'+list+'</a>';
-        var html = '<a href="farmer.html" onClick=" "><div class="about"><img src="'+e.Name+'"></img><p class="name">'+e.Name+'</p><p>'+list+'</p></div></a>';
+        var html = '<a href="farmer.html" onClick=" "><div class="about"><img src="'+e.Farmer_Pic.url+'"></img><p class="name">'+e.Name+'</p><p>'+list+'</p></div></a>';
         $('.tab_content').append(html);
       });
       //document.getElementById('content').innerHTML = templates.catalogTemplate(objList);
