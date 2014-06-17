17a118a910609053cfa609af8650d972e5949f35
hhheyaidn <hhheyaidn26@gmail.com>
2014-06-17 17:53:49 +0800

    test

--- js/product.js
+++ js/product.js
@@ -27,76 +27,101 @@ $(function(){
 	}).find('a').focus(function(){
 		this.blur();
 	});
-	$('.123').click(function(){
-		getData(1,'北部');
-	});
-	$('.456').click(function(){
-		getData(1,'五穀雜糧');
-	});
 });
 
-function getData(page,category){
-
+function getData(page,category,tab){
   var limit = 15;
   var skip = (page-1) * limit;
   var Farmer = Parse.Object.extend("Farmer");
   var Product = Parse.Object.extend("Product");
   var query = new Parse.Query(Farmer);
   var queryP = new Parse.Query(Product);
-  //================LIST!!!
-  var list = "hihi";
-  //================
+  var list="";
+
   query.limit(limit);
   query.skip(skip);
   query.equalTo("district", category);
   query.descending("createdAt");
   query.find({
     success: function(results) {
-      //alert(1);
       $('.tab_content').html("");
       var objList = results.map(function (e){ return e.toJSON() });
+      //console.log(objList);
       objList.forEach(function (e){
         queryP.descending("createdAt");
         queryP.equalTo("Farmer",e.objectId);
         queryP.find({
           success: function(output){
             var productList = output.map(function (e){ return e.toJSON() });
-            console.log(productList);
+            //console.log(productList);
+            list="";
             productList.forEach(function (e){
-              //alert(2);
-              list = list+e.Prod_name+" ";
+             list += e.Prod_name+" ";
             });
+          },
+          error: function(error) {
+          alert("Error: " + error.code + " " + error.message);
           }
+        }).then(function(){
+          var id = "'"+e.objectId+"'";
+          var html = '<a href="farmer.html?name='+e.objectId+'"><div class="about"><img src="'+e.Farmer_Pic.url+'"></img><p class="name">'+e.Name+'</p><p>'+list+'</p></div></a>';
+          $('.tab_content').append(html);
         });
-        //var photo = e.Farmer_Pic;
-        //$("profileImg")[0].src = profilePhoto.url();
-        //var html = '<a href="farmer.html"><img src="img/about_2.png"></img></a><a href="farmer.html" id="name">'+e.Name+'</a><br><a href="farmer.html" id="product">'+list+'</a>';
-        var html = '<a href="farmer.html" onClick=" "><div class="about"><img src="'+e.Name+'"></img><p class="name">'+e.Name+'</p><p>'+list+'</p></div></a>';
-        $('.tab_content').append(html);
       });
-      //document.getElementById('content').innerHTML = templates.catalogTemplate(objList);
+      //底下的小分頁=================
       query.limit(0);
       query.skip(0);
       var option = {};
-      // To support pagination.
       query.count({
         success: function(count){
         var totalPage = Math.ceil(count / limit);
         var currentPage = parseInt(page);
         option = {
-          // Watch out the limit.
           'previous': (currentPage === 1) ? 1 : currentPage-1,
           'next': (currentPage === totalPage) ? currentPage : currentPage+1,
           'current': currentPage,
           'last': totalPage,
         };
-        //document.getElementById('pagination').innerHTML = 
-        //templates.catalogPaginationTemplate(option);
         }, 
         error: function(err){
 
         }  
       });
+      //===========================
+    }
+  });
+  event.preventDefault();
+}
+
+
+function call (id){
+  var Farmer = Parse.Object.extend("Farmer");
+  var Product = Parse.Object.extend("Product");
+  var query = new Parse.Query(Farmer);
+  var queryP = new Parse.Query(Product);
+  query.equalTo("objectId", id);
+  query.find({
+    success: function(results) {
+      var objList = results.map(function (e){ return e.toJSON() });
+      queryP.descending("createdAt");
+      queryP.equalTo("Farmer",id);
+      queryP.find({
+        success: function(output){
+          var productList = output.map(function (e){ return e.toJSON() });
+          productList.forEach(function (e){
+            var pro = '<div class="product"><img src="'+e.Prod_Pic.url+'"></img><h3>'+e.Prod_name+'</h3><br><a class="name">'+e.Prod_stat+'</a><p>$'+e.Prod_price+'</p></div>';
+            $('#product-list').append(pro);
+            console.log(pro);
+          })
+        }
+      }).then(function(){
+          var banner = '<img src="'+objList[0].Farmer_Pic.url+'" class="farmer_head"><h2 class="slogan"><div id="farm-name">'+objList[0].Name+'</div><div id="farmer-name">'+objList[0].Name+'</div></h2>';
+          $('.banner').append(banner);
+          var description = '<p>'+objList[0].farm_story+'</p>';
+          $('.description').append(description);
+          var info = '<p><i class="fa fa-home fa-2x"></i><a href="'+objList[0].website+'">'+objList[0].Name+'</a></p><p><i class="fa fa-facebook-square fa-2x"></i><a href="'+objList[0].facebook+'">粉絲專頁</a></p><p><i class="fa fa-phone fa-2x"></i>'+objList[0].telephone+'</p>';
+          $('.info').append(info);
+        });
     }
   });
   event.preventDefault();
